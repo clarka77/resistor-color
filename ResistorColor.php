18526a65ff0af01beeb3423d79b665a482ac966b
Clark <clark@clarka.me>
2023-03-25 18:16:03 -0500

    all good

--- ResistorColor.php
+++ ResistorColor.php
@@ -24,7 +24,19 @@
 
 declare(strict_types=1);
 
-function colorCode(string $color): int
-{
-    throw new \BadFunctionCallException("Implement the colorCode function");
+const COLORS = array(
+    0 => 'black',
+    1 => 'brown',
+    2 => 'red',
+    3 => 'orange',
+    4 => 'yellow',
+    5 => 'green',
+    6 => 'blue',
+    7 => 'violet',
+    8 => 'grey',
+    9 => 'white',
+);
+
+function colorCode(string $color): int {
+    return array_flip(COLORS)[$color];
 }
